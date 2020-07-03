95159609b6c489d3880766a5c557fcfc236c5e9c
Isaac Freund <ifreund@ifreund.xyz>
2020-07-03 15:32:17 +0200

    bitmask: implement

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,10 @@ export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
 
+test "" {
+    _ = @import("bit_mask.zig");
+}
+
 test "basic add functionality" {
     testing.expect(add(3, 7) == 10);
 }
